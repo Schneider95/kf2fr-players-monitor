fd7b525e853baf456fe26577be7152e9715b28be
Arnaud <arnaud.sartori.as@gmail.com>
2016-06-11 09:57:18 +0200

    Ajout du Sharpshooter et de deux maps

--- public/app/config/stats-config.ts
+++ public/app/config/stats-config.ts
@@ -5,6 +5,6 @@ export class StatsConfigService {
 	public timePlayedNeededForKf2FrHoe = 18000;
 	public nbPerksMaxNeeded= 6;
 	public nbHoeWonNeeded = 4;
-	public nbSuicidalWonNeeded = 8;
-	public nbHardWonNeeded = 10;
+	public nbSuicidalWonNeeded = 9;
+	public nbHardWonNeeded = 11;
 }
