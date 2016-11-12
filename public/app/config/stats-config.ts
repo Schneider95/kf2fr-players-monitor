d84ef457f75cfa3ed93046372ed7ab19333ec953
Arnaud <arnaud.sartori.as@gmail.com>
2016-11-12 10:54:11 +0100

    correction stats

--- public/app/config/stats-config.ts
+++ public/app/config/stats-config.ts
@@ -2,9 +2,9 @@ import {Injectable} from 'angular2/core';
 
 @Injectable()
 export class StatsConfigService {
-	public timePlayedNeededForKf2FrHoe = 21000;
-	public nbPerksMaxNeeded= 6;
-	public nbHoeWonNeeded = 5;
+	public timePlayedNeededForKf2FrHoe = 18000;
+	public nbPerksMaxNeeded = 5;
+	public nbHoeWonNeeded = 4;
 	public nbSuicidalWonNeeded = 11;
 	public nbHardWonNeeded = 12;
 }
